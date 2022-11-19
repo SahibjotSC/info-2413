ccb487fa370de179c29af6b9900720df02222ab3
SahibjotSC <118297014+SahibjotSC@users.noreply.github.com>
2022-11-18 21:00:29 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>qBudget Management System</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<?php
+		$username = '';
+		$message = '';
+		$type = '';
+
+		if ($_GET['username'] ?? null) $username = $_GET['username'];
+
+		if(isset($_GET['loginerror']))
+		{
+			$type = 'login';
+			if($_GET['loginerror'] == 'sqlfail')
+			{
+				$message = '<h6 class="error">*Failed to connect to MySQL</h6>';
+			}
+			else if ($_GET['loginerror'] == 'missingvalues')
+			{
+				$message = '<h6 class="error">*Please input all required values</h6>';
+			}
+			else if ($_GET['loginerror'] == 'incorrect')
+			{
+				$message = '<h6 class="error">*Incorrect username and/or password</h6>';
+			}
+		}
+		else if(isset($_GET['registererror']))
+		{
+			$type = 'register';
+			if($_GET['registererror'] == 'connectionfailed')
+			{
+				$message = '<h6 class="error">*Failed to connect to MySQL</h6>';
+			}
+			else if ($_GET['registererror'] == 'incomplete')
+			{
+				$message = '<h6 class="error">*Please complete the registration form</h6>';
+			}
+			else if ($_GET['registererror'] == 'empty')
+			{
+				$message = '<h6 class="error">*Please complete the registration form</h6>';
+			}
+			else if ($_GET['registererror'] == 'invalidusername')
+			{
+				$message = '<h6 class="error">*Invalid Username</h6>';
+			}
+			else if ($_GET['registererror'] == 'retypepass')
+			{
+				$message = '<h6 class="error">*Passwords do not match</h6>';
+			}
+			else if ($_GET['registererror'] == 'passwordlength')
+			{
+				$message = '<h6 class="error">*Password must be between 5 and 20 characters long</h6>';
+			}
+			else if ($_GET['registererror'] == 'userexist')
+			{
+				$message = '<h6 class="error">*Username exists, please choose another</h6>';
+			}
+		}
+		else if (isset($_GET['signup']) == 'success')
+		{
+			$type = 'register';
+			$message = '<h6 class="success">*Signup Successful. Please login</h6>';
+		}
+		else if (isset($_GET['signup']) == 'failed')
+		{
+			$type = 'register';
+			$message = '<h6 class="error">*Signup Failed</h6>';
+		}
+		?>
+		<div class="hero">
+			<div class="form-box">
+				<div class="button-box">
+					<div id="button"></div>
+					<button type="button" class="toggle-button" onclick="login()">Login</button>
+					<button type="button" class="toggle-button" onclick="register()">Register</button>
+				</div>
+				<script>register();</script>
+				<form id="login" class="input-group" action="login.php" method="post">
+					<input type="text" class="input-field" placeholder="Username" name="username" required value=<?php echo $username; ?>>
+					<input type="password" class="input-field" placeholder="Password" name="password" required>
+					<?php if ($type == 'login') echo $message; ?>
+					<button type="submit" class="submit-button">Login</button>
+				</form>
+				<form id="register" class="input-group" action="register.php" method="post">
+					<input type="text" class="input-field" placeholder="Username" name="username" required value=<?php echo $username; ?>>
+					<input type="password" class="input-field" placeholder="Password" name="password" required>
+					<input type="password" class="input-field" placeholder="Repeat Password" name="retypepassword" required>
+					<?php if ($type == 'register') echo $message; ?>
+					<input type="submit" class="submit-button" value="Register">
+				</form>
+			</div>
+		</div>
+		<script>
+			var x = document.getElementById("login");
+			var y = document.getElementById("register");
+			var z = document.getElementById("button");
+
+			function register() {
+				x.style.left = "-400px"
+				y.style.left = "50px"
+				z.style.left = "110px"
+			}
+			
+			function login() {
+				x.style.left = "50px"
+				y.style.left = "450px"
+				z.style.left = "0"
+			}
+		</script>
+		<?php 
+		if ($type == 'register') echo '<script type="text/javascript"> register(); </script>';
+		else if ($type == 'login') echo '<script type="text/javascript"> login(); </script>';
+		?>
+	</body>
+</html>
---
+++ register.php
@@ -0,0 +1,69 @@
+<?php
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'phplogin';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	// If there is an error with the connection, stop the script and display the error.
+	header("Location: index.php?registererror=connectionfailed");
+	exit();
+	
+	//exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+// Now we check if the data was submitted, isset() function will check if the data exists.
+if (!isset($_POST['username'], $_POST['password'], $_POST['retypepassword'])) {
+	// Could not get the data that should have been sent.
+	header("Location: index.php?registererror=incomplete");
+	exit();
+}
+// Make sure the submitted registration values are not empty.
+if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['retypepassword'])) {
+	// One or more values are empty.
+	header("Location: index.php?registererror=empty");
+	exit();
+}
+if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
+    header("Location: index.php?registererror=invalidusername");
+	exit();
+}
+if ($_POST['password'] != $_POST['retypepassword']) {
+	header("Location: index.php?registererror=retypepass&username=".$_POST['username']);
+	exit();
+}
+if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+	header("Location: index.php?registererror=passwordlength&username=".$_POST['username']);
+	exit();
+}
+// We need to check if the account with that username exists.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	$stmt->store_result();
+	// Store the result so we can check if the account exists in the database.
+	if ($stmt->num_rows > 0) {
+		// Username already exists
+		header("Location: index.php?registererror=userexist");
+	} else {
+		// Username doesnt exists, insert new account
+		if ($stmt = $con->prepare('INSERT INTO accounts (username, password) VALUES (?, ?)')) {
+			// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+			$stmt->bind_param('ss', $_POST['username'], $password);
+			$stmt->execute();
+			header("Location: index.php?signup=success");
+		} else {
+			// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+			header("Location: index.php?signup=failed");
+		}
+	}
+	$stmt->close();
+} else {
+	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+	header("Location: index.php?signup=failed");
+}
+$con->close();
+?>
